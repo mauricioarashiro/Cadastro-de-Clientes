bede049a8a5a0d4c3a56fd483ddca046bd1f6bdf
mauricioarashiro <45978902+mauricioarashiro@users.noreply.github.com>
2018-12-18 18:19:24 -0200

    Add files via upload

--- br_gerarRelatorio.php
+++ br_gerarRelatorio.php
@@ -2,13 +2,7 @@
 <?php include_once 'includes/menu.inc.php' ?>
 
 <?php
-	$utf8 = header("Content-Type: text/html; charset=utf-8");
-	$servidor = "bdhost0087.servidorwebfacil.com";
-	$usuario = "brpermut_kenji";
-	$senha = "92e2dd5bffUrso"; 
-	$bancodedados = "brpermut_sistema"; 
 	
-	//$link = mysqli_connect ('bdhost0087.servidorwebfacil.com', 'brpermut_kenji', '92e2dd5bffUrso', 'brpermut_sistema');
 	
 	// Criando a conexão
 	$conn = new mysqli($servidor, $usuario, $senha, $bancodedados);
